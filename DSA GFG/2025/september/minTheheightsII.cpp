c45c1e33db29ac07e64b5df56f1776b16b74c699
Rohan Naidu <rohannaidu940@gmail.com>
2025-09-12 10:36:24 -0500

    initial commit

---
+++ DSA GFG/2025/september/minTheheightsII.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        // code here
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        int ans=arr[n-1]-arr[0];
+        for(int i=0;i<n-1;i++){
+            int mini=min(arr[0]+k,arr[i+1]-k);
+            int maxi=max(arr[n-1]-k,arr[i]+k);
+            if(mini<0) continue;
+            ans=min(ans,maxi-mini);
+        }
+        return ans;
+    }
+};
