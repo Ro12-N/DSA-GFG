856f849af12251938fe12beb1d1bde03b7a6c4cc
Ro12-N <rohannaidu940@gmail.com>
2025-12-14 18:50:31 +0530

    intial commit

---
+++ DSA GFG/2025/DECEMBER/2DSubmatrixSumQueries.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    vector<int> prefixSum2D(vector<vector<int>> &mat, vector<vector<int>> &queries) {
+        // code here
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>pre(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                pre[i][j]=mat[i][j];
+                if(i>0) pre[i][j]+=pre[i-1][j];
+                if(j>0) pre[i][j]+=pre[i][j-1];
+                if(i>0 &&j>0) pre[i][j]-=pre[i-1][j-1];
+            }
+        }
+        vector<int>ans;
+        for(auto q:queries){
+            int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
+            int curr=pre[r2][c2];
+            if(r1>0) curr-=pre[r1-1][c2];
+            if(c1>0) curr-=pre[r2][c1-1];
+            if(r1>0 &&c1>0) curr+=pre[r1-1][c1-1];
+            ans.push_back(curr);
+        }
+        return ans;
+    }
+};
+// Time Complexity: O(N*M + Q)
+// Space Complexity: O(N*M)
