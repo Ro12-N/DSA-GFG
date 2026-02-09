d0d4c4f4a98d5370abe806d332775dd454810148
ro12-n <rohannaidu940@gmail.com>
2026-02-10 01:18:46 +0530

    initial commit

--- 2026/FEBRUARY/findKthRotation.cpp
+++ 2026/FEBRUARY/findKthRotation.cpp
@@ -11,3 +11,29 @@ class Solution {
 };
 // Time Complexity: O(logN) where N is the size of the input array
 // Space Complexity: O(1)
+
+
+// Another approach using binary search
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        // Code Here
+        int n=arr.size();
+        int s=0;
+        int e=n-1;
+        while(s<=e){
+            if(arr[s]<=arr[e]) return s;
+            int mid=s+(e-s)/2;
+            int nxt=(mid+1)%n;
+            int prev=(mid-1+n)%n;
+            if(arr[mid]<=arr[nxt] && arr[mid]<=arr[prev]) return mid;
+            
+            
+            if(arr[mid]<=arr[e]) e=mid-1;
+            else s=mid+1;
+        }
+        return 0;
+    }
+};
+// Time Complexity: O(logN) where N is the size of the input array
+// Space Complexity: O(1)
