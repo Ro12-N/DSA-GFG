6a3ae762402633cd1908004347272e3a239efadb
ro12-n <rohannaidu940@gmail.com>
2026-07-22 23:18:42 +0530

    Binary Search

---
+++ 2026/JULY/maximumReachableIndexDifference.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int maxIndexDifference(string &s) {
+        vector<int> best(26, -1);
+
+        int ans = -1;
+
+        for (int i = (int)s.size() - 1; i >= 0; i--) {
+
+            int c = s[i] - 'a';
+
+            int reach;
+
+            if (c == 25) {
+                reach = i;
+            }
+            else if (best[c + 1] != -1) {
+                reach = best[c + 1];
+            }
+            else {
+                reach = i;
+            }
+
+            best[c] = max(best[c], reach);
+
+            if (c == 0) {
+                ans = max(ans, reach - i);
+            }
+        }
+
+        return ans;
+    }
+};
+// Time Complexity: O(n) where n is the length of the string s
+// Space Complexity: O(1) since the best array has a fixed size of 26
